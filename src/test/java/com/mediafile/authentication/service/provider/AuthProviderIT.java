c28cc5a8b68d17c79c8cf0fb8c4b56df561e0a9f
diegomated1 <diegocardwr@gmail.com>
2024-03-22 21:51:54 -0600

    test: verify jwt

--- src/test/java/com/mediafile/authentication/service/provider/AuthProviderIT.java
+++ src/test/java/com/mediafile/authentication/service/provider/AuthProviderIT.java
@@ -4,6 +4,9 @@
  */
 package com.mediafile.authentication.service.provider;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.mediafile.authentication.service.rmi.RMIServer;
 import com.mediafile.rmi.classes.Response;
 import com.mediafile.rmi.classes.User;
@@ -14,12 +17,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import java.util.UUID;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -63,29 +61,59 @@ public class AuthProviderIT {
 
     @org.junit.jupiter.api.Test
     public void testAuth() throws Exception {
-        Response<String> responseRegister = authProvider.Register(new RegisterArgs("user2024", "user2024@gmail.com", "user2024pass"));
+        String uuid = UUID.randomUUID().toString();
+        
+        // REGISTER
+        RegisterArgs regArgs = new RegisterArgs(uuid, "user name", "user2024@gmail.com", "user2024pass");
+        Response<String> responseRegister = authProvider.Register(regArgs);
 
+        DecodedJWT decoded = JWT.decode(responseRegister.getData());
+        String userId = decoded.getClaim("userId").asString();
+        String email = decoded.getClaim("email").asString();
+        decoded.getClaim("fullname").asString();
+        
         assertTrue(responseRegister.isSuccess());
         assertEquals(0, responseRegister.getErrors().length);
-        assertTrue(responseRegister.getData().matches("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"));
+        assertTrue(userId.matches("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"));
+        assertTrue(email.matches("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"));
         
+        // AUTH REGISTER JWT
         Response<Boolean> responseAuthRegister = authProvider.Auth(responseRegister.getData());
         
         assertTrue(responseAuthRegister.isSuccess());
         assertEquals(0, responseAuthRegister.getErrors().length);
         assertTrue(responseAuthRegister.getData());
         
-        Response<String> responseLogin = authProvider.Login(new LoginArgs("user2024", "user2024pass"));
+        // LOGIN
+        Response<String> responseLogin = authProvider.Login(new LoginArgs("user2024@gmail.com", "user2024pass"));
         
-        assertTrue(responseLogin.isSuccess());
-        assertEquals(0, responseLogin.getErrors().length);
-        assertTrue(responseLogin.getData().matches("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"));
+        DecodedJWT _decoded = JWT.decode(responseRegister.getData());
+        String _userId = _decoded.getClaim("userId").asString();
+        String _email = _decoded.getClaim("email").asString();
+        decoded.getClaim("fullname").asString();
         
+        assertTrue(responseRegister.isSuccess());
+        assertEquals(0, responseRegister.getErrors().length);
+        assertTrue(_userId.matches("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"));
+        assertTrue(_email.matches("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"));
+        
+        // AUTH LOGIN JWT
         Response<Boolean> responseAuthLogin = authProvider.Auth(responseLogin.getData());
         
         assertTrue(responseAuthLogin.isSuccess());
         assertEquals(0, responseAuthLogin.getErrors().length);
         assertTrue(responseAuthLogin.getData());
+        
+        // GET USER BY ID
+        Response<User> responseGetUser = authProvider.GetUser(uuid);
+        User user = responseGetUser.getData();
+        
+        assertTrue(responseGetUser.isSuccess());
+        assertEquals(0, responseGetUser.getErrors().length);
+        assertEquals(user.getId(), uuid);
+        assertEquals(user.getFullName(), "user name");
+        assertEquals(user.getEmail(), "user2024@gmail.com");
+        
     }
     
 }
