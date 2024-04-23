29bef6c9736791f38e3e731c53f57c2e2cb50fb5
Acux-dev <juansup123@gmail.com>
2024-04-23 16:26:12 -0500

    FEAT: sql id y email unicos

--- db/create_database.sql
+++ db/create_database.sql
@@ -3,10 +3,10 @@ CREATE DATABASE Auth;
 USE Auth;
 
 CREATE TABLE User (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY UNIQUE NOT NULL,
     fullName VARCHAR(120) NOT NULL,
-    email VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
     creationDate DATETIME default current_timestamp,
-    ultimaActividad DATETIME default current_timestamp,
+    ultimaActividad DATETIME default current_timestamp
 );
